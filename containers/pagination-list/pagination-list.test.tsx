3c87b2e155245d87de6a4f3ca381ada4d3734a99
Adrian <adrian@ufounders.com>
2021-12-26 18:31:20 +0100

    [FIX] -.-

--- containers/pagination-list/pagination-list.test.tsx
+++
@@ -1,9 +0,0 @@
-// import { screen, render } from '@testing-library/react';
-
-// beforeEach(() => render(<div>hola</div>));
-
-// describe('Pagination List container', () => {
-//   it('Must display basic title', () => {
-//     // expect(screen.queryByText(/Pagination List/i)).toBeInTheDocument();
-//   });
-// });
